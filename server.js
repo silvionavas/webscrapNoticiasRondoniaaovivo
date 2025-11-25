a64a3be6eeeab48a9f485cd7650bf723fe700113
Silvio Lopes <silviopln@hotmail.com>
2025-11-25 00:57:56 -0300

    server fix

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ app.get("/noticias", async (req, res) => {
   }
 });
 
-app.listen(3000, () =>
-  console.log("Rodando! http://localhost:3000/noticias")
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Rodando na porta " + PORT);
+});
